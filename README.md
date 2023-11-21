04465bf544b0bfa7d4683ebe323f6574dae0ddeb
Achchala Deepan <achchala2003@gmail.com>
2023-11-20 20:29:39 -0500

    project commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# IntelliMed QueryBot
+
+### PointClickCare's AI Hackathon Project
+
+AI-powered chatbot leveraging language models to simplify the extraction of data from Electronic Health Record (EHR)
+databases and provide critical insights, streamlining the interaction between health professionals and EHR
+
+### Next Steps!
+
+- [ ] Fine-tuning LLama-2 for this specific use case; this approach would've required more time for implementation, but may yield better results
--- tools/openai.py
+++ tools/openai.py
@@ -1,19 +1,19 @@
-#Note: The openai-python library support for Azure OpenAI is in preview.
+# Note: The openai-python library support for Azure OpenAI is in preview.
 import json
 import os
-import openai
+import tools.openai as openai
 import tools.files as files
 
 openai.api_type = "azure"
 openai.api_base = os.getenv("OPENAI_API_BASE")
 openai.api_version = os.getenv("OPENAI_API_VERSION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai_engine=os.getenv("OPENAI_ENGINE")
+openai_engine = os.getenv("OPENAI_ENGINE")
 
 schema = files.get_schema()
 sample_data = files.get_sampel_data()
 
-sqlContent = f'''
+sqlContent = f"""
   You are an RDBMS and SQL expert.
   You use the SQL schema provided between backquotes to provide SQL queries to provide the data requested by the user.
   `{schema}`
@@ -37,49 +37,52 @@ sqlContent = f'''
   - patients visits to facilities are stored in the [dbo].[clients] table.
   Use the following data between backquotes for looking up vital names:
   `{sample_data}`
-  '''
+  """
 
 
 def get_sql(userContent):
     response = openai.ChatCompletion.create(
-    engine=openai_engine,
-    messages = [
-        {"role":"system","content":sqlContent},
-        {"role":"user","content":userContent},
+        engine=openai_engine,
+        messages=[
+            {"role": "system", "content": sqlContent},
+            {"role": "user", "content": userContent},
         ],
-    temperature=0.7,
-    max_tokens=800,
-    top_p=0.95,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=None)
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
+    )
 
     # print(response)
-    response0 = response.choices[0].message.content.strip('`')
+    response0 = response.choices[0].message.content.strip("`")
     sql = json.loads(response0)["sql"]
     # sql = response0.strip().strip("`")
     return sql
 
+
 def provide_response(question, db_data):
-    sysContent = f'''
+    sysContent = f"""
     You are a health professional.
     I received the data between double backquotes from the database in response to the question between backquotes:
     question: `{question}`
-    response: ``{db_data}``'''
+    response: ``{db_data}``"""
 
-    userContent = '''Use the data from the question and response and rewrite the response in a human readable form.'''
+    userContent = """Use the data from the question and response and rewrite the response in a human readable form."""
     response = openai.ChatCompletion.create(
-    engine=openai_engine,
-    messages = [
-        {"role":"system","content":sysContent},
-        {"role":"user","content":userContent},
+        engine=openai_engine,
+        messages=[
+            {"role": "system", "content": sysContent},
+            {"role": "user", "content": userContent},
         ],
-    temperature=0.7,
-    max_tokens=800,
-    top_p=0.95,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=None)
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
+    )
 
     # print(response)
     response0 = response.choices[0].message.content
